63fec247c4c66f301d69792513e414d4dd3ee3a9
NicoHoareau <nicolas.hoareau.pro@orange.fr>
2018-03-06 09:01:34 +0100

    activité fiche créée

---
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/appliwikimonsterlegends/FicheActivity.java
@@ -0,0 +1,15 @@
+package fr.nicolashoareau_toulousewcs.appliwikimonsterlegends;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FicheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fiche);
+    }
+}
